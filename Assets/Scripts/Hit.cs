9ed79f03caf8d1a4c0946f2b5aa6681e2b169bcf
Timur Nikolaev <yasnovadishu@gmail.com>
2020-07-30 11:52:45 +0900

    simple movement

---
+++ Assets/Scripts/Hit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    [SerializeField] private float fireRate = 0.3f;
+    [SerializeField] private int damage = 1;
+    [SerializeField] private Transform firePoint;
+    private float timer;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= fireRate)
+        {
+            if (Input.GetButton("Fire2"))
+            {
+                timer = 0f;
+                FireGun();
+            }
+        }
+    }
+    private void FireGun()
+    {
+        Ray ray = new Ray(firePoint.position, firePoint.forward);
+        RaycastHit hitInfo;
+        if(Physics.Raycast(ray, out hitInfo, 50))
+        {
+            var health = hitInfo.collider.GetComponent<Health>();
+            if(health != null)
+            {
+                health.TakeDamage(damage);
+            }
+        }
+    }
+     
+}
