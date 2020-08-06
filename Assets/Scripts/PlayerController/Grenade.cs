5d0d06df9d41159ec73f3a8e0665d5b74cd9cb85
VitaliiVoloshin <vitalii.voloshin@mytona.com>
2020-08-06 16:16:30 +0900

    fix proj

---
+++ Assets/Scripts/PlayerController/Grenade.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float delay = 3f;
+    float countdown;
+    bool hasExploded = false;
+    public GameObject explosionEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if(countdown<=0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        //eff.SetActive(true);
+        Destroy(gameObject);
+        Invoke("MyFunction", 10);
+        
+    }
+    void MyFunction()
+    {
+        explosionEffect.SetActive(false);
+
+
+    }
+}
