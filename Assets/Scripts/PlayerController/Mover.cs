d815a6cbbc3a1748e82e15578cf01ec059b55f57
VitaliiVoloshin <vitalii.voloshin@mytona.com>
2020-08-06 16:49:06 +0900

    v2

---
+++ Assets/Scripts/PlayerController/Mover.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Shooter.PlayerController
+{
+    public class Mover : MonoBehaviour
+    {
+        private CharacterController characterController;
+        private Animator animator;
+        [SerializeField] private float moveSpeed = 100;
+        private void Awake()
+        {
+            characterController = GetComponent<CharacterController>();
+            animator = GetComponentInChildren<Animator>();
+        }
+        void Update()
+        {
+            Movement();
+            transform.LookAt(LookAtCursor());
+        }
+        private void Movement()
+        {
+            if(!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A)
+                && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+            {
+                return;
+            }
+            var hor = Input.GetAxis("Horizontal");
+            var vert = Input.GetAxis("Vertical");
+            var movement = new Vector3(hor, 0, vert);
+            characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
+            animator.SetFloat("forwardSpeed", movement.magnitude);
+        }
+        private Vector3 LookAtCursor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit);
+            if (hasHit)
+            {
+                Vector3 tarPos = new Vector3(hit.point.x, 0, hit.point.z);
+                return tarPos;
+            }
+            return Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController/Player.cs
@@ -0,0 +1,25 @@
+﻿using Shooter.PlayerController;
+using UnityEngine;
+
+namespace Shooter.PlayerController
+{
+    public class Player : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<Mover>();
+            GetComponent<CreateWeapon>().Create();
+        }
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                 GetComponent<CreateWeapon>().Switch(CreateWeapon.Slot.First);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                GetComponent<CreateWeapon>().Switch(CreateWeapon.Slot.Second);
+            }
+        }
+    }
+}
