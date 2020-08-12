9a2bcaa4ed98d567b1cfd2a55cbd238420ce6df7
Timur Nikolaev <yasnovadishu@gmail.com>
2020-08-12 12:14:59 +0900

    commit

---
+++ Assets/Scripts/UiController/KillFeedBoard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Shooter.UiController
+{
+    public class KillRecord
+    {
+        public Sprite weaponSprite;
+        public string killer;
+        public string killed;
+    }
+    public class KillFeedBoard : MonoBehaviour
+    {
+        public static KillFeedBoard instance;
+        public RecordPanel[] recordPanels;
+        public Dictionary<int, KillRecord> dictOfKillRecords = new Dictionary<int, KillRecord>();
+        private void Awake()
+        {
+            instance = this;
+        }
+        public void AddKillToDict(KillRecord killRecord)
+        {
+            dictOfKillRecords.Add(dictOfKillRecords.Count, killRecord);
+            AddRecordToKillFeedBoard(killRecord, recordPanels);
+        }
+        public void AddRecordToKillFeedBoard(KillRecord killRecord, RecordPanel[] recordBoard)
+        {
+            if(recordBoard.Length >= dictOfKillRecords.Count)
+            {
+                recordBoard[dictOfKillRecords.Count - 1].gameObject.SetActive(true);
+            }
+            int i = recordBoard.Length - 1;
+            while(i > 0)
+            {
+                recordBoard[i].SetPanelInfo(recordBoard[i - 1].GetPanelInfo());
+                i--;
+            }
+            recordBoard[0].SetPanelInfo(killRecord);
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/UiController/RecordPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Shooter.UiController
+{
+    
+    public class RecordPanel : MonoBehaviour
+    {
+        public Image weaponSprite;
+        public Text killer;
+        public Text killed;
+        public void SetPanelInfo(KillRecord _killRecord)
+        {
+            killer.text= _killRecord.killer;
+            killed.text = _killRecord.killed;
+            weaponSprite.sprite = _killRecord.weaponSprite;
+        }
+        public KillRecord GetPanelInfo()
+        {
+            return new KillRecord { killer = killer.text, killed = killed.text, weaponSprite = weaponSprite.sprite };
+        }
+    }
+}
